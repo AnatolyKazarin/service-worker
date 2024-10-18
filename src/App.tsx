eac520c7653cc7534f39a66c8b9b18ae396d0329
Anatoly Kazarin <alkidkaz@gmail.com>
2024-10-18 07:41:41 +0300

    v4

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,55 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {createPortal} from "react-dom";
+import ModalContent from "./Modal";
 
 function App() {
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+            console.log('Service Worker зарегистрирован.');
+
+            // if (registration.waiting) {
+            //     notifyUserAboutUpdate(); // Уведомление, если новый SW ждет активации
+            // }
+
+            if (registration) {
+                registration.addEventListener("updatefound", () => {
+                    console.log("Service Worker update found!");
+                    notifyUserAboutUpdate();
+                });
+            }
+
+            // registration.onupdatefound = () => {
+            //     const installingWorker = registration.installing;
+            //     if(installingWorker) {
+            //         installingWorker.onstatechange = () => {
+            //             if (installingWorker.state === 'installed') {
+            //                 if (navigator.serviceWorker.controller) {
+            //                     notifyUserAboutUpdate(); // Сообщение об обновлении
+            //                 }
+            //             }
+            //         };
+            //     }
+            // };
+        });
+
+        navigator.serviceWorker.addEventListener('message', (event) => {
+            if (event.data.type === 'NEW_VERSION_AVAILABLE') {
+                notifyUserAboutUpdate();
+            }
+        });
+    }
+
+    function notifyUserAboutUpdate() {
+        console.log('notifyUserAboutUpdate');
+        createPortal(
+            <ModalContent />,
+            document.body
+        )
+    }
+
   const [count, setCount] = useState(0)
 
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -5,50 +5,6 @@ import './index.css'
 import {createPortal} from "react-dom";
 import ModalContent from "./Modal";
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('Service Worker зарегистрирован.');
-
-        // if (registration.waiting) {
-        //     notifyUserAboutUpdate(); // Уведомление, если новый SW ждет активации
-        // }
-
-        if (registration) {
-            registration.addEventListener("updatefound", () => {
-                console.log("Service Worker update found!");
-                notifyUserAboutUpdate();
-            });
-        }
-
-        // registration.onupdatefound = () => {
-        //     const installingWorker = registration.installing;
-        //     if(installingWorker) {
-        //         installingWorker.onstatechange = () => {
-        //             if (installingWorker.state === 'installed') {
-        //                 if (navigator.serviceWorker.controller) {
-        //                     notifyUserAboutUpdate(); // Сообщение об обновлении
-        //                 }
-        //             }
-        //         };
-        //     }
-        // };
-    });
-
-    navigator.serviceWorker.addEventListener('message', (event) => {
-        if (event.data.type === 'NEW_VERSION_AVAILABLE') {
-            notifyUserAboutUpdate();
-        }
-    });
-}
-
-function notifyUserAboutUpdate() {
-    console.log('notifyUserAboutUpdate');
-    createPortal(
-        <ModalContent />,
-        document.body
-    )
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
