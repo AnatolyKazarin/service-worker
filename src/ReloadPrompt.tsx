8f389be07635bb5a1e8be97eb6df3168aeeb0229
Anatoly Kazarin <alkidkaz@gmail.com>
2024-10-21 20:30:18 +0300

    update

--- src/ReloadPrompt.tsx
+++ src/ReloadPrompt.tsx
@@ -45,10 +45,10 @@ function ReloadPrompt() {
       { (offlineReady || needRefresh)
       && (
         <div className="ReloadPrompt-toast">
-          <div className="ReloadPrompt-message">
+          <div className="ReloadPrompt-message" style={{color: "black"}}>
             { offlineReady
               ? <span>App ready to work offline</span>
-              : <span>New content available, click on reload button to update!!.</span>}
+              : <span>New content available, click on reload button to update.</span>}
           </div>
           { needRefresh && <button className="ReloadPrompt-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button> }
           <button className="ReloadPrompt-toast-button" onClick={() => close()}>Close</button>
