35f3670a25d613b79d980fcb2df31aace77555ea
Anatoly Kazarin <alkidkaz@gmail.com>
2024-10-21 09:31:05 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -55,6 +55,15 @@ function App() {
     // }
 
   const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        navigator.serviceWorker
+            .getRegistrations()
+            .then((regs) => {
+                console.log('REgs', regs.length);
+                regs.forEach((reg) => reg.update())
+            });
+    }, [count]);
     // console.log('Show modal', showModal)
   return (
     <>
