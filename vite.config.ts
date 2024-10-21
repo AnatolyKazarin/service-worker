ad2a2d0c509dbc3ca2543e651d7c04dd62e1092f
Anatoly Kazarin <alkidkaz@gmail.com>
2024-10-21 08:50:32 +0300

    icon fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       react(),
+      viteStaticCopy({
+          targets: [
+              {
+                  src: 'public',
+                  dest: ''
+              }
+          ]
+      }),
       VitePWA(  {
           // srcDir: "src",
           // filename: "service-worker.js",
@@ -16,7 +25,7 @@ export default defineConfig({
               globPatterns: ['**/*.{js,css,html,ico,png,svg}']
           },
           manifest: {icons: [{
-              src: './src/assets/free.png',
+              src: '/public/free.png',
                   sizes: '512x512',
                   type: 'image/png',
                   purpose: 'any maskable'
